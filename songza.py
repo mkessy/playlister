5ca5761ef4684fd5c04ef8cd3b1a9f17be68a295
Mepuka Kessy <kessy002@umn.edu>
2013-07-22 00:22:33 -0500

    finished playlist downloading function

--- songza.py
+++ songza.py
@@ -1,8 +1,14 @@
 import time
 import requests
 import simplejson as json
+import logging
 import pprint
 import pickle
+import tempfile
+
+from math import log, ceil
+from Queue import PriorityQueue
+from collections import defaultdict
 
 #script to download playlists from songza
 
@@ -25,16 +31,22 @@ SUB_BROWSE_CATS = {
                    'record_store_clerk':'record_store_clerk'
                    }
 
-TEST_STATIONIDS = ['1724409', '1392809'] #some stationids used for testing methods
-
 HEADER = {'User-Agent':
           "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)"}
 
-REQUEST_KWARGS = {'headers':HEADER, 'timeout':5.0, 'allow_redirects':False}
+REQUEST_KWARGS = {'headers':HEADER, 'timeout':10.0, 'allow_redirects':False}
+
+HTTPError = requests.exceptions.HTTPError
+logging.basicConfig(filename='test_log2.log', level=logging.DEBUG)
 
 
 #BEGIN UTILITY FUNCTIONS
-HTTPError = requests.exceptions.HTTPError
+
+def expected_tries(N):
+    """Calculate the expected number of tries to get
+    0.999% coverage of the given playlist"""
+
+    return int(ceil( log(0.001)/log( (float(N)-1)/N ) ))
 
 def get_station_info(stationid):
     """Returns a dictionary with relevant station info"""
@@ -48,65 +60,152 @@ def get_station_info(stationid):
     station_info = res.json()
     return station_info
 
+#END UTILITY FUNCTIONS
 
-def get_songs(stationid):
+def get_songs(stationid, song_count, song_names):
     """Attempts to track all songs from the given stationid"""
 
     next_song_url = BASE_SONG % (stationid,)
 
-    try:
-        station_info = get_station_info(stationid)
-    except HTTPError as e:
-        print 'Error opening stationid: %s' % stationid
-        raise
-
     songs = []
-    song_names = []
-    song_count = station_info['song_count']
-#    max_tries = song_count + 10
-    max_tries = 5
-    SLEEP_TIME = 5
+    max_tries = 10
+    #max_tries = expected_tries(song_count)
+    SLEEP_TIME = .25
 
     tries = 0
-    while len(songs) < song_count and tries < max_tries:
-        print 'Attempt: %s of %s\n' % (tries, max_tries)
+    song_session = requests.Session()
 
-        print '\nRequesting new song...'
-        song_res = requests.get(next_song_url, **REQUEST_KWARGS)
-        if song_res.status_code == 200:
-            song_json = song_res.json()
-            song_name = song_json['song']['title']
-            if song_name in song_names:
-                print 'Song already in list!\n\n'
-            else:
-                print 'Added \"%s\" to the list!\n\n' % (song_name,)
-                songs.append(song_json)
-                song_names.append(song_name)
+    logging.debug('==================================================')
+    logging.debug('Starting request loop for station: %s' % stationid)
+    try:
 
-        else:
-            print 'Not OK server response\n'
-            print 'Status: %s\n' % (song_res.status_code)
-            print 'Will retry..\n\n'
 
-        time.sleep(3)
-        tries += 1
-    print '\n=======================\n'
-    print 'Finished with main loop...\n'
-    print 'Returning list with %s songs of a possible %s\n' % (len(songs), song_count)
-    return {'complete':True, 'songs':songs} if len(songs)==song_count else {'complete':False, 'songs':songs}
+        while len(song_names) < song_count and tries < max_tries:
+            print 'Attempt: %s of %s\n' % (tries, max_tries)
+
+            print '\nRequesting new song...',
+            try:
+                #song_response = song_session.get(next_song_url, **REQUEST_KWARGS)
+
+                song_response = requests.get(next_song_url, **REQUEST_KWARGS)
+                #for rate limiting
+                #if song_response.status_code == 420 or 429:
+                #    print 'Rate limit.. Destroying cookie'
+                #    song_session.close() #close session to delete cookie
+                #    song_session = requests.Session()
+                #    song_response = song_session.get(next_song_url, **REQUEST_KWARGS)
+
+                if song_response.status_code == 200:
+                    song_json = song_response.json()
+                    song_name = song_json['song']['title']
+                    if song_name in song_names:
+                        print 'Song already in list!\n'
+                    else:
+                        print 'Added \"%s\" to the list!\n' % (song_name,)
+                        songs.append(song_json)
+                        song_names.append(song_name)
+
+                else:
+                    print 'Not OK server response'
+                    print 'Status: %s' % (song_reponse.status_code)
+                    print 'Will retry..\n\n'
+                    tries -= 1
+
+                time.sleep(SLEEP_TIME)
+                tries += 1
+            except requests.Timeout as e:
+                print 'Request timed out'
+                print 'Retrying request...'
+                print repr(e)
+                continue
+
+        print '\n=======================\n'
+        print 'Finished with main loop...\n'
+        print 'Returning list with %s songs of a possible %s\n' % (len(song_names), song_count)
+
+    except Exception as e:
+        print 'Ended loop prematurely because of error.\n'
+        print 'Returning incomplete playlist'
+        logging.debug('**************ERROR***************')
+        logging.debug(repr(e))
+        print e.value
+        raise
+    finally:
+        complete = len(songs)==song_count
+        return ( {'complete':complete, 'remaining':song_count-len(songs),
+            'song_names':song_names, 'song_count':song_count, 'stationid':stationid, 'songs':songs}  )
+
+def get_playlists(stationids):
+    """Gets the playlists specified in the stationids list, and saves them to file"""
+
+    print 'Initializing queue with %s stations' % len(stationids)
+    station_queue = PriorityQueue()
+
+    song_list_counter = defaultdict(list) #keeps track of the songs currently in each song list
+    station_info = defaultdict(int)
+    song_list_store = {} #intermediate store of song lists while queue is still running
+
+
+    temp_file_dict = {}
+
+
+    for stationid in stationids:
+        try:
+            station_info[stationid] = get_station_info(stationid)['song_count']
+        except HTTPError as e:
+            print 'Error could not add station %s' % (stationid,)
+            continue
+    #initialize the queue and temp files
+    for stationid, song_count in station_info.items():
+        station_queue.put( (song_count, stationid) )
+        tf = tempfile.NamedTemporaryFile(prefix=stationid, delete=False)
+        temp_file_dict[stationid] = tf.name
+
+
+    while not station_queue.empty():
+
+        print '-----'
+        remaining, stationid = station_queue.get()
+        print '\nAttempting to get songs from station: %s\n' % (stationid,)
+        current_station_status = get_songs(stationid,
+                station_info[stationid],
+                song_list_counter[stationid])
+                #update tempfile
+
+        with open(temp_file_dict[stationid], 'w+') as tf:
+            tf.write(json.dumps(current_station_status))
+
+    playlists = {}
+    for stationid, temp in temp_file_dict.items():
+        with open(temp) as tf:
+            playlists[stationid] = json.loads(tf.read())
+
+    #merge all playlists (stored in temp files) into a single file
+    with open(stationid+'_playlists.json', 'w+') as f:
+        json.dump(playlists, f)
+
+    #add return value summarizing the processed
+
+def tests():
+    """Not yet implemented"""
+
+    test_stations = ['1724409', '1392809', '1391300', '1387704', '1501119']
+    get_playlists(test_stations)
 
 def main():
+    #generate files for testing
+    test_stations = ['1724409', '1392809', '1391300', '1387704', '1501119']
+    test_station_info = {station:get_station_info(station) for station in test_stations}
 
-    songs = get_songs('1724409')
-
-    pprint.pprint(songs['songs'])
-
-    with open('1724409.json', 'w') as f:
-        json.dump(songs['songs'], f)
-
-    with open('1724409.txt', 'w') as f:
-        pickle.dump(songs['songs'], f)
-
+    for station in test_stations:
+        print '\n********************************************'
+        print 'Attempting to get songs from station: %s' % station
+        print   '********************************************\n'
+        songs = get_songs(station, test_station_info[station]['song_count'], [])
+        name = 'test3_'+station+'.json'
+        with open(name, 'w+') as f:
+            json.dump({'stationid':station, 'data':songs}, f)
 
 if __name__ == '__main__':
-    main()
+    #main()
+    tests()
